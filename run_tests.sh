cb0035c37dfb2bf4bec45261842bcd6b2d359671
kumarsibi <kumarsibi0303@gmail.com>
2019-04-22 11:03:21 +0100

    Update run_tests.sh
    changes

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e 
 
-. ~/.virtualenvs/python2.7/bin/activate
+#. ~/.virtualenvs/python2.7/bin/activate
+. ~/.virtualenvs/bin/activate
 
 PYTHONPATH=. python -m pystache.commands.test
